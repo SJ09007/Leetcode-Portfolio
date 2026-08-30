153e96d1c314e8ca872d334a886e77ff9bdd52e9
SJ07 <sj0900777@gmail.com>
2026-08-30 13:43:05 +0530

    Time: 24 ms (39.58%) | Memory: 33.4 MB (90.94%) - LeetSync

--- 2212-removing-minimum-and-maximum-from-array/removing-minimum-and-maximum-from-array.py
+++ 2212-removing-minimum-and-maximum-from-array/removing-minimum-and-maximum-from-array.py
@@ -1,12 +1,11 @@
 class Solution:
     def minimumDeletions(self, nums: List[int]) -> int:
-        n = len(nums)
 
         MinVar = float('inf')
         MaxVar = float('-inf')
         minMaxIndexList = [0, 0]
 
-        for i in range(n):
+        for i in range(len(nums)):
             if nums[i] < MinVar:
                 MinVar = nums[i]
                 minMaxIndexList[0] = i
@@ -15,17 +14,15 @@ class Solution:
                 MaxVar = nums[i]
                 minMaxIndexList[1] = i
 
-        left = min(minMaxIndexList)
-        right = max(minMaxIndexList)
+        return min(max(minMaxIndexList)+1,len(nums)-min(minMaxIndexList) , (len(nums)-max(minMaxIndexList))+(min(minMaxIndexList)+1) )
 
-        # 3 possibilities:
-        # 1. Remove both from front
-        front = right + 1
+'''
+since integers are distinct is given , can we just take 2 vars 
+Minvar=inf
+MaxVar=-inf
+and a list minMaxIndexList=[0,0]
+traverse through the array and store/update indices of min and max var 
+then 3 possibilites hogi , ya toh max(minMaxIndexList)+1 , ya toh len(nums)-min(minMaxIndexList) , ya toh (len(nums)-max(minMaxIndexList))+(min(minMaxIndexList)+1)
 
-        # 2. Remove both from back
-        back = n - left
-
-        # 3. Remove one from front and one from back
-        both = (left + 1) + (n - right)
-
-        return min(front, back, both)
+inn teeno ka min answer hoga
+'''
